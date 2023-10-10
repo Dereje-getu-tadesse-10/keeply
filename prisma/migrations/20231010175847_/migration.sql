8d388954531c30d5c6e8e9414ed1726a48fd46d7
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-10 20:11:34 +0200

    update: collection and item model

---
+++ prisma/migrations/20231010175847_/migration.sql
@@ -0,0 +1,84 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpires` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerId` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerType` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessToken` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `VerificationRequest` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[provider,providerAccountId]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `provider` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `Account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropIndex
+DROP INDEX "Account_providerId_providerAccountId_key";
+
+-- DropIndex
+DROP INDEX "Session_accessToken_key";
+
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "accessToken";
+ALTER TABLE "Account" DROP COLUMN "accessTokenExpires";
+ALTER TABLE "Account" DROP COLUMN "createdAt";
+ALTER TABLE "Account" DROP COLUMN "providerId";
+ALTER TABLE "Account" DROP COLUMN "providerType";
+ALTER TABLE "Account" DROP COLUMN "refreshToken";
+ALTER TABLE "Account" DROP COLUMN "updatedAt";
+ALTER TABLE "Account" ADD COLUMN     "access_token" STRING;
+ALTER TABLE "Account" ADD COLUMN     "expires_at" INT4;
+ALTER TABLE "Account" ADD COLUMN     "id_token" STRING;
+ALTER TABLE "Account" ADD COLUMN     "provider" STRING NOT NULL;
+ALTER TABLE "Account" ADD COLUMN     "refresh_token" STRING;
+ALTER TABLE "Account" ADD COLUMN     "scope" STRING;
+ALTER TABLE "Account" ADD COLUMN     "session_state" STRING;
+ALTER TABLE "Account" ADD COLUMN     "token_type" STRING;
+ALTER TABLE "Account" ADD COLUMN     "type" STRING NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Session" DROP COLUMN "accessToken";
+ALTER TABLE "Session" DROP COLUMN "createdAt";
+ALTER TABLE "Session" DROP COLUMN "updatedAt";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt";
+ALTER TABLE "User" DROP COLUMN "updatedAt";
+
+-- DropTable
+DROP TABLE "VerificationRequest";
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" STRING NOT NULL,
+    "token" STRING NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
