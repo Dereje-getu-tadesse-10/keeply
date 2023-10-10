d9eab79e9152de2113e16661577dec319fbb611a
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-10 19:39:08 +0200

    update: init authjs and update all pkg

---
+++ env.d.ts
@@ -0,0 +1,16 @@
+
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            GITHUB_CLIENT_ID: string;
+            GITHUB_CLIENT_SECRET: string;
+            GOOGLE_CLIENT_ID: string;
+            GOOGLE_CLIENT_SECRET: string;
+            NEXTAUTH_URL: string;
+            NEXT_AUTH_SECRET: string;
+        }
+    }
+}
+
+export {}
---
+++ src/lib/auth.ts
@@ -0,0 +1,30 @@
+import type { NextAuthOptions } from "next-auth";
+
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const {
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET, 
+    NEXTAUTH_SECRET
+} = process.env;
+
+export const options: NextAuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
+        }),
+        GoogleProvider({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    secret: NEXTAUTH_SECRET,
+    session: {
+        strategy: "jwt",
+    }
+};
+
