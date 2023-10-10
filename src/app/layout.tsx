b4c3afe67373a42c7b0270368919fec07a1664fe
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-10 20:24:10 +0200

    update: group page and add lucide-react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Gabarito } from "next/font/google"
 import { NextAuthProvider } from './provider'
 
-const inter = Inter({ subsets: ['latin'] })
+const gabarito = Gabarito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="fr">
+      <body className={gabarito.className}>
         <NextAuthProvider>
           {children}
         </NextAuthProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-"use client";
-
-import { signIn } from "next-auth/react";
 
 export default function Home() {
   return (
     <main>
-      <button
-        onClick={() => signIn("google")}
-      >
-          Gooooogle
-      </button>
-      <button
-        onClick={() => signIn("github")}
-      >
-         Githuuuuub
-      </button>
+
     </main>
   )
 }
