046bd9a0ec8a027e2ecc58caa638504ed8a75c27
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-14 11:39:39 +0200

    udpate: login page

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -2,8 +2,17 @@ import styles from "./page.module.css"
 import Image from "next/image"
 import Link from "next/link"
 import { Auth } from "$/components/forms/index"
+import { auth } from "$/lib/auth"
+import { redirect } from "next/navigation"
 
-export default function Login() {
+export default async function Login() {
+
+    const session = await auth()
+
+    if(session) {
+        redirect("/dashboard")
+    }
+
     return (
         <main className={styles.main}>
             <header className={styles.main__header}>
