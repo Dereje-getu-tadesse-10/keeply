8b524f0fee9c413c4a7be768f60055307f90dcbc
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-25 10:45:39 +0200

    update: collectible schema

--- src/schemas/collectibles-schema.ts
+++ src/schemas/collectibles-schema.ts
@@ -1,8 +1,12 @@
 import { z } from 'zod';
 
 const collectiblesSchema = z.object({
-    name: z.string(),
-    description: z.string(),
+    name: z.string()
+        .min(1, 'Le nom est trop court')
+        .max(50, 'Le nom est trop long'),
+    description: z.string()
+        .min(1, 'La description est obligatoire')
+        .max(500, 'La description est trop longue'),
     collectionId: z.string(),
     status: z.enum(['ACQUIRED', 'PLANNED']),
     dragPosition: z.number(),
