74d53551e6807f4e5edef49fc86948c187cf58dc
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-27 16:12:29 +0200

    update: add buttonLink component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import { Button } from '$/components/ui';
+import { ButtonLink } from '$/components/ui/button-link/button-link';
 
 const Home = () => {
   return (
     <main>
-      <Button intent='primary' size='small'>
-        Button
-      </Button>
-      <br />
-      <Button intent='primary' size='medium'>
-        Button
-      </Button>
-      <br />
-      <Button>Button</Button>
+      <ButtonLink intent={'primary'} size={'medium'} href='/about'>
+        Bonjour le monde
+      </ButtonLink>
     </main>
   );
 };
---
+++ src/components/ui/button-link/button-link.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import styles from './button-link.module.css';
+import Link from 'next/link';
+
+const buttonLink = cva(styles['button-link'], {
+  variants: {
+    intent: {
+      primary: styles.primary,
+    },
+    size: {
+      small: styles.small,
+      medium: styles.medium,
+    },
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
+  },
+});
+
+export interface LinkButtonProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    VariantProps<typeof buttonLink> {}
+
+export const ButtonLink: React.FC<LinkButtonProps> = ({
+  intent,
+  size,
+  ...props
+}) => (
+  <Link
+    passHref={true}
+    href={props.href || '/'}
+    className={buttonLink({ intent, size })}
+    {...props}
+  />
+);
