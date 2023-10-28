9b5a860eada99a4684be85a6b197f8cddb8cf88d
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-28 17:03:20 +0200

    update: modalStore

--- src/stores/useModalStore.ts
+++ src/stores/useModalStore.ts
@@ -1,11 +1,28 @@
+// import { create } from 'zustand';
+
+// type ModalState = {
+//   isOpen: boolean;
+//   toggleModal: () => void;
+// };
+
+// export const useModalStore = create<ModalState>((set) => ({
+//   isOpen: false,
+//   toggleModal: () => set((state) => ({ isOpen: !state.isOpen })),
+// }));
+
 import { create } from 'zustand';
 
 type ModalState = {
-  isOpen: boolean;
-  toggleModal: () => void;
+  modals: Record<string, boolean>;
+  toggleModal: (id: string) => void;
 };
 
 export const useModalStore = create<ModalState>((set) => ({
-  isOpen: false,
-  toggleModal: () => set((state) => ({ isOpen: !state.isOpen })),
+  modals: {},
+  toggleModal: (id) => set((state) => ({
+    modals: {
+      ...state.modals,
+      [id]: !state.modals[id],
+    }
+  })),
 }));
