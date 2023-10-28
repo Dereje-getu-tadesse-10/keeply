f721ef8b2c63327b2041754a91d04581f87e7530
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-28 10:57:11 +0200

    feat: route /update/username

---
+++ src/app/api/users/update/username/route.ts
@@ -0,0 +1,62 @@
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { prisma } from '$/lib/prisma';
+
+import { config } from '$/lib/auth';
+import { verifySession } from '$/lib/verify-session';
+import { createUsernameSchema } from '$/schemas/users-schema';
+
+export async function PUT(req: Request, res: Response) {
+    const session = await getServerSession(config);
+    const body = await req.json();
+    const response = createUsernameSchema.safeParse(body);
+
+    // Si le body n'est pas conforme au schéma, on renvoie une erreur
+    if (!response.success) {
+        const errorObj = JSON.parse(response.error.message)[0];
+        const errorMessage = errorObj.message;
+        return NextResponse.json(
+            { message: errorMessage },
+            { status: 400 }
+        );
+    }
+
+    // On vérifie que la session est valide
+    const sessionError = verifySession(session, { userId: response.data.userId });
+    if (sessionError) {
+        console.log(sessionError);
+        return NextResponse.json(sessionError, { status: sessionError.status });
+    }
+
+    // On récupère les données de la collection
+    const { username, userId } = response.data;
+
+    const userExists = await prisma.user.findUnique({
+        where: {
+            username: username,
+        }
+    });
+
+    if (userExists) {
+        return NextResponse.json(
+            { message: `Le nom d'utilisateur est déjà utilisé` },
+            { status: 400 }
+        );
+    }
+
+    // On crée la collection
+    await prisma.user.update({
+        where: {
+            id: userId,
+        },
+        data: {
+            username,
+        }
+    });
+
+    // On renvoie une réponse
+    return NextResponse.json(
+        { message: `Le nom d'utilisateur a bien été modifié` },
+        { status: 201 }
+    );
+}  
