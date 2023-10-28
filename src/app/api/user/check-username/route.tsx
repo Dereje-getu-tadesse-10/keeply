1703a5b07434cf7a7c140edc30d627b0b19dd75d
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-10-28 10:47:28 +0200

    feat: route /check-username

---
+++ src/app/api/user/check-username/route.tsx
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '$/lib/prisma';
+
+import { usernameSchema } from '$/schemas/users-schema';
+
+export async function POST(req: Request, res: Response) {
+    const body = await req.json();
+    const response = usernameSchema.safeParse(body);
+    
+        if (!response.success) {
+            const errorObj = JSON.parse(response.error.message)[0];
+            const errorMessage = errorObj.message;
+            return NextResponse.json(
+                {message: errorMessage},
+                { status: 400 }
+            );
+        }
+        
+
+    const { username } = response.data
+
+    const usernameExists = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    })
+
+    if(usernameExists){
+        return NextResponse.json({ status: 200, data: { exists: true } });
+    }
+    
+    return NextResponse.json({ status: 200, data: { exists: false } });
+
+  }
