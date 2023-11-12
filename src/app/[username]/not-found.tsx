88be43b70b3840b2f08fff7daffaf58bf0c27c80
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-11-12 13:00:42 +0100

    fix: typo in not-found page

--- src/app/[username]/not-found.tsx
+++ src/app/[username]/not-found.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 const NotFound = () => (
   <div className={styles.not_found}>
     <h2>Oups</h2>
-    <p>La page que vous recherchez n'existe pas ou a été déplacée.</p>
+    <p>La page que vous recherchez n&apos;existe pas ou a été déplacée.</p>
   </div>
 );
 
