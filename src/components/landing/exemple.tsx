5a709677c6abff8602297162eea205c53816a1dd
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-11-12 17:50:05 +0100

    update: remove unnecessary code in Exemple component

--- src/components/landing/exemple.tsx
+++ src/components/landing/exemple.tsx
@@ -16,20 +16,4 @@ const data = [
 
 // Keeply vous permet de gérer vos collections en toute simplicité. Ajoutez, modifiez et supprimez vos collections en quelques clics.
 
-export const Exemple = () => (
-  <div className={styles.exemple}>
-    {data.map((item) => (
-      <div className={styles.exemple__content}>
-        <h2>{item.title}</h2>
-        <p>{item.description}</p>
-      </div>
-    ))}
-    <h1>Commencez maintenant</h1>
-    <p>
-      N&apos;attendez pas une seconde de plus pour commencer à gérer vos
-      collections de vinyles, de cartes ou de timbres avec Keeply. Il est temps
-      de montrer au monde vos pièces rares!
-    </p>
-    <ButtonLink href='/login'>Commencer maintenant</ButtonLink>
-  </div>
-);
+export const Exemple = () => <div className={styles.exemple}></div>;
